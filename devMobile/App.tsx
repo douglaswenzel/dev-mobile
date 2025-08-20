27a4bbe87cc92e1546b8dd70ab713ea4f8207b0b
Douglas Wenzel <115610413+douglaswenzel@users.noreply.github.com>
2025-08-20 19:53:14 -0300

    Atualiza o componente App: modifica o texto e adiciona imagem do aluno

--- devMobile/App.tsx
+++ devMobile/App.tsx
@@ -1,11 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Meu projeto em React Native na matéria de Desenvolvimento Mobile na Fatec Votorantim</Text>
+      <Text style={styles.redText}>Aluno: </Text>
+      <Image
+        source={{ uri: "https://avatars.githubusercontent.com/u/115610413?v=4" }}
+        style={{ width: 200, height: 200, borderRadius: 50, margin: 15 }}
+      />
+      <Text style={styles.redText}>Douglas Wenzel</Text>
     </View>
   );
 }
@@ -13,8 +18,13 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#878721',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  redText: {
+    color: 'red',
+    fontSize: 25,
+    margin: 15
+  }
 });
