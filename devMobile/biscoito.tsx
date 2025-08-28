974951dda9357038a71270e6b4be0c55a0d298f5
Douglas Wenzel <115610413+douglaswenzel@users.noreply.github.com>
2025-08-27 22:04:55 -0300

    Atualização de estilos para a página do biscoito da sorte

--- devMobile/biscoito.tsx
+++ devMobile/biscoito.tsx
@@ -42,10 +42,11 @@ export class Biscoito extends Component<any, BiscoitoState>{
       <View style={styles.container}>
           <Image 
             source={this.state.img}
+            style={styles.img}
           />
-          <Text>{this.state.textoFrase}</Text>
-          <TouchableOpacity onPress={this.quebraBiscoito}>
-            <Text>Quebrar Biscoito</Text>
+          <Text style={styles.textoFrase}>{this.state.textoFrase}</Text>
+          <TouchableOpacity onPress={this.quebraBiscoito} style={styles.botao}>
+            <Text style={styles.btnArea}>Quebrar Biscoito</Text>
           </TouchableOpacity>
       </View>
     );
@@ -59,6 +60,29 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  img: {
+    width: 250,
+    height: 250
+  },
+  textoFrase: {
+    fontSize: 20,
+    color: '#DD7B22',
+    margin: 30,
+    fontStyle: 'italic',
+    textAlign: 'center',
+  },
+  botao: {
+    width: 230,
+    height: 50,
+    borderWidth: 2,
+    borderColor: '#dd7b22',
+    borderRadius: 25
+  },
+  btnArea: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'center',
+  }
 });
 
 export default Biscoito;
