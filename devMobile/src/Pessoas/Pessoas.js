fa5fee8f090de6277bd1e49145d44189afc25338
Douglas Wenzel <115610413+douglaswenzel@users.noreply.github.com>
2025-09-03 19:45:00 -0300

    Adiciona novo componente Pessoas com estilo e mensagem inicial

---
+++ devMobile/src/Pessoas/Pessoas.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import { View, Text, Stylesheet } from 'react=native';
+
+class Pessoas extends Component { 
+    render(){
+        return(
+          <View style={styles.areaPessoa}>
+            <Text>Olá!!! Esta é a primeira versão deste meu componente chamado pessoas!</Text>
+          </View>
+        );
+    }
+}
+
+const styles = Stylesheet.create({
+    areaPessoa: {
+        backgroundColor: '#222',
+        height: 150,
+        marginBottom: 25,
+        padding: 15,
+    },
+    textoPessoa: {
+        color: '#FFF',
+    }
+})
+
+export default Pessoas;
