d3ede6c026b49ac4f2e03c64ade38bf9f5fc3dd4
Douglas Wenzel <115610413+douglaswenzel@users.noreply.github.com>
2025-09-03 19:55:15 -0300

    Adiciona exibição de Nome, Idade e E-mail no componente Pessoas

--- devMobile/src/Pessoas/Pessoas.js
+++ devMobile/src/Pessoas/Pessoas.js
@@ -6,9 +6,9 @@ class Pessoas extends Component {
         return(
           <View style={styles.areaPessoa}>
             <Text>Olá!!! Esta é a primeira versão deste meu componente chamado pessoas!</Text>
-            <Text style={styles.textoPessoa}>Nome: </Text>
-            <Text style={styles.textoPessoa}>Idade: </Text>
-            <Text style={styles.textoPessoa}>E-mail: </Text>
+            <Text style={styles.textoPessoa}>Nome: {this.props.data.nome}</Text>
+            <Text style={styles.textoPessoa}>Idade: {this.props.data.idade}</Text>
+            <Text style={styles.textoPessoa}>E-mail: {this.props.data.email}</Text>
 
           </View>
         );
