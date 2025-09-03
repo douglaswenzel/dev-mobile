db78a1181121123e954f322db4c68f39806b8455
Douglas Wenzel <115610413+douglaswenzel@users.noreply.github.com>
2025-09-03 20:03:37 -0300

    Atualização de erros e semanticas

--- devMobile/src/Pessoas/Pessoas.js
+++ devMobile/src/Pessoas/Pessoas.js
@@ -1,21 +1,20 @@
 import React, { Component } from 'react';
-import { View, Text, Stylesheet } from 'react=native';
+import { View, Text, StyleSheet } from 'react-native';
 
 class Pessoas extends Component { 
     render(){
         return(
           <View style={styles.areaPessoa}>
-            <Text>Olá!!! Esta é a primeira versão deste meu componente chamado pessoas!</Text>
+            <Text>Entradas realizadas</Text>
             <Text style={styles.textoPessoa}>Nome: {this.props.data.nome}</Text>
             <Text style={styles.textoPessoa}>Idade: {this.props.data.idade}</Text>
             <Text style={styles.textoPessoa}>E-mail: {this.props.data.email}</Text>
-
           </View>
         );
     }
 }
 
-const styles = Stylesheet.create({
+const styles = StyleSheet.create({
     areaPessoa: {
         backgroundColor: '#222',
         height: 150,
