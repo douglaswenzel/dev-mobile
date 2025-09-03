598ca522461f9f9d61d6b480e6ee8e0e0d25bd73
Douglas Wenzel <115610413+douglaswenzel@users.noreply.github.com>
2025-09-03 20:32:36 -0300

    Adiciona botão para registrar saída no componente Pessoas

--- devMobile/src/Pessoas/Pessoas.js
+++ devMobile/src/Pessoas/Pessoas.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Button, TouchableOpacity } from 'react-native';
 
 class Pessoas extends Component { 
     render(){
@@ -9,6 +9,13 @@ class Pessoas extends Component {
             <Text style={styles.textoPessoa}>Nome: {this.props.data.nome}</Text>
             <Text style={styles.textoPessoa}>Idade: {this.props.data.idade}</Text>
             <Text style={styles.textoPessoa}>E-mail: {this.props.data.email}</Text>
+            <TouchableOpacity 
+              activeOpacity={0.90}
+              style={styles.btnSaida}> 
+              <Text style={ {color: 'white'}}>
+                REGISTRAR SAÍDA
+             </Text>
+            </TouchableOpacity>
           </View>
         );
     }
@@ -28,6 +35,13 @@ const styles = StyleSheet.create({
         color: 'white',
         fontsize: 95,
         fontWeight: 500
+    },
+    btnSaida: {
+        color: 'red',
+        fontSize: 25,
+        backgroundColor: 'red',
+        width: 80,
+        height: 40
     }
 })
 
