618fcb58a2aff08da3a55aa02206f61824922f5b
Douglas Wenzel <115610413+douglaswenzel@users.noreply.github.com>
2025-09-03 20:06:34 -0300

    Adiciona exibição da data de entrada no componente Pessoas

--- devMobile/src/Pessoas/Pessoas.js
+++ devMobile/src/Pessoas/Pessoas.js
@@ -5,7 +5,7 @@ class Pessoas extends Component {
     render(){
         return(
           <View style={styles.areaPessoa}>
-            <Text>Entradas realizadas</Text>
+            <Text style={styles.textoArea} >Entrada realizada em: 15/set às 08h45</Text>
             <Text style={styles.textoPessoa}>Nome: {this.props.data.nome}</Text>
             <Text style={styles.textoPessoa}>Idade: {this.props.data.idade}</Text>
             <Text style={styles.textoPessoa}>E-mail: {this.props.data.email}</Text>
@@ -23,6 +23,11 @@ const styles = StyleSheet.create({
     },
     textoPessoa: {
         color: '#FFF',
+    },
+    textoArea: {
+        color: 'white',
+        fontsize: 95,
+        fontWeight: 500
     }
 })
 
