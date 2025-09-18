f1877da687b4f0dae6842ae934adef2bcdbd9d14
Douglas Wenzel <115610413+douglaswenzel@users.noreply.github.com>
2025-09-17 21:00:32 -0300

    Feat(Desafio): Chamada a lógica de navegação dentro do arquivo app(aulasete)

--- devMobile/aulaSete.tsx
+++ devMobile/aulaSete.tsx
@@ -2,7 +2,8 @@ import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import HomeScreen from './src/screens/HomeScreen';
-import DetailsScreen from './src/screens/DetailsScreen';
+import ConsultaScreen from './src/screens/ConsultaScreen';
+import CadastroScreen from './src/screens/CadastroScreen';
 
 const Stack = createStackNavigator();
 
@@ -11,7 +12,8 @@ export default function AulaSete() {
         <NavigationContainer>
             <Stack.Navigator initialRouteName='Home'>
                 <Stack.Screen name="Home" component={HomeScreen} />
-                <Stack.Screen name="Details" component={DetailsScreen} />
+                <Stack.Screen name="Cadastro" component={CadastroScreen} />
+                <Stack.Screen name="Consulta" component={ConsultaScreen} />
             </Stack.Navigator>
         </NavigationContainer>
     )
