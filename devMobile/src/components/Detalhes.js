e9dfcc71612f70b4d6807933547d888b029b6d78
Douglas Wenzel <115610413+douglaswenzel@users.noreply.github.com>
2025-09-24 22:17:41 -0300

    Feat(Detalhes): Componente parcial de detalhes do projeto de cestas

---
+++ devMobile/src/components/Detalhes.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Image, StyleSheet, View, TouchableOpacity } from 'react-native';
+
+
+
+
+
+
+const estilos = StyleSheet.create({
+    nome: {
+        color: "#464646",
+        fontSize: 26,
+        lineHeight: 42,
+        fontWeight: 'bold',
+    },
+    fazenda: { 
+      flexDirection: 'row',
+      paddingVertical: 12,
+    },
+    nomeFazenda: {
+        fonrSize: 16,
+        lineHeight: 26,
+        marginLeft: 12
+    }
+})
